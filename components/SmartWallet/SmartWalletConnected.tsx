b4c6301ef8e1f5e4aa8d70d7969a8e5e5858b8de
Abhijeet Sinha <abhijeet.sinha383@gmail.com>
2023-06-17 10:04:55 +0530

    added erc1155 display

--- components/SmartWallet/SmartWalletConnected.tsx
+++ components/SmartWallet/SmartWalletConnected.tsx
@@ -3,11 +3,14 @@ import {
   useAddress,
   useBalance,
   Web3Button,
+  ThirdwebNftMedia,
+  useContract,
+  useOwnedNFTs
 } from "@thirdweb-dev/react";
 import React from "react";
 import {
   activeChain,
-  nftDropAddress,
+  editionDrop_Address,
   tokenAddress,
 } from "../../const/constants";
 import { Signer } from "ethers";
@@ -29,6 +32,15 @@ const SmartWalletConnected: React.FC<ConnectedProps> = ({ signer }) => {
 const ClaimTokens = () => {
   const address = useAddress();
   const { data: tokenBalance } = useBalance(tokenAddress);
+
+  const {
+    contract
+  } = useContract(editionDrop_Address);
+  const {
+    data: ownedNFTs,
+    isLoading:ownedNFTsIsLoading
+  } = useOwnedNFTs(contract,address)
+
   return (
     <div>
       <p>Smart Wallet Address: {address}</p>
@@ -43,12 +55,31 @@ const ClaimTokens = () => {
       <br />
       <h1>Claim NFT:</h1>
       <Web3Button
-        contractAddress={nftDropAddress}
+        contractAddress={editionDrop_Address}
         action={(contract) => contract.erc1155.claim(0, 1)}
       >
         Claim NFT
       </Web3Button>
-      
+      <br /> <br />
+      {ownedNFTsIsLoading ? (
+        <p>Loading...</p>
+      ) :(
+        <div>
+          {ownedNFTs && ownedNFTs.length > 0 ?(
+            ownedNFTs.map((nft) => (
+              <div>
+                <ThirdwebNftMedia
+                  metadata = {nft.metadata}
+                  />
+                  <p>{nft.metadata.name}</p>
+                  <p>QTY:{nft.quantityOwned}</p>
+              </div>
+            ))
+          ) :(
+            <p>You have no NFTs</p>
+          )}
+        </div>
+      )}
     </div>
   );
 };
